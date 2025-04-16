2d56e60c96d4378cf3321e55ac4058bd5f8347cc
gpancot <gpancot@hotmail.com>
2025-04-16 23:46:11 +0200

    estetica

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Command for airsonic/airsonic-advanced
 
 file config_airsonic.py
 
-server_url="https://airsonic_address:port/airsonic/rest"
-username="username"
-password="password"
+server_url='https://airsonic_address:port/airsonic/rest'
+username='username'
+password='password'
--- airsonic_refresh.py
+++ airsonic_refresh.py
@@ -4,22 +4,22 @@ import config_airsonic as config
 
 # Genera l'URL per l'API
 # rest_url = f'{config.server_url}/ping.view'
-rest_url = f"{config.server_url}/startScan.view"
+rest_url = f'{config.server_url}/startScan.view'
 
 # Parametri necessari per l'autenticazione
 params = {
-    "u": config.username,
-    "p": config.password,
-    "v": "1.15.0",  # Versione API, modificabile
-    "c": "Myapp",  # Nome client, personalizzabile
-    "f": "json"  # Formato della risposta
+    'u': config.username,
+    'p': config.password,
+    'v': '1.15.0',  # Versione API, modificabile
+    'c': 'Myapp',  # Nome client, personalizzabile
+    'f': 'json'  # Formato della risposta
 }
 
 # Invio della richiesta per avviare la scansione delle cartelle multimediali
 response = requests.get(rest_url, params=params)
 
 if response.status_code != 200:
-    print(f"Errore durante la connect: {response.json()}")
+    print(f'Errore durante la connect: {response.json()}')
     exit()
 
 resp_js = response.json()
